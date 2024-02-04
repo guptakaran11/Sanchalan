5da0b989ca2ce2bdbf4cc0054fd8e66df3ba10dd
guptakaran11 <gupta.karan1162@gmail.com>
2024-02-04 19:59:57 +0530

    modified:   sanchalan/lib/ride/controller/provider/tripProvider/rideRequestProvider.dart 	modified:   sanchalan/pubspec.lock 	modified:   sanchalan/pubspec.yaml

--- sanchalan/lib/ride/controller/provider/tripProvider/rideRequestProvider.dart
+++ sanchalan/lib/ride/controller/provider/tripProvider/rideRequestProvider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sanchalan/common/model/directionModel.dart';
 
@@ -15,5 +16,10 @@ class RideRequestProvider extends ChangeNotifier {
   updateDirection(DirectionModel newDirection) {
     directionDetails = newDirection;
     notifyListeners();
+  }
+
+  decodePolyLineAndUpdatePolyLineField() {
+    PolylinePoints polylinePoints = PolylinePoints();
+    polylineCoordinatesList.clear();
   }
 }
