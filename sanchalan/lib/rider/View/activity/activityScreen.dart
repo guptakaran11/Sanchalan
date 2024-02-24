8e9d5b6336ad8a03e3898ecf4d8f94d722ba356f
guptakaran11 <gupta.karan1162@gmail.com>
2024-02-24 21:46:59 +0530

    activity screen changes

--- sanchalan/lib/rider/View/activity/activityScreen.dart
+++ sanchalan/lib/rider/View/activity/activityScreen.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: file_names
 
+import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:sanchalan/common/model/rideRequestModel.dart';
+import 'package:sanchalan/constant/constants.dart';
 import 'package:sanchalan/constant/utils/colors.dart';
 import 'package:sanchalan/constant/utils/textstyle.dart';
 import 'package:sizer/sizer.dart';
@@ -14,6 +20,9 @@ class ActivityScreenRider extends StatefulWidget {
 }
 
 class _ActivityScreenRiderState extends State<ActivityScreenRider> {
+  DatabaseReference tripHistoryRef = FirebaseDatabase.instance
+      .ref()
+      .child('RideHistoryRider/${auth.currentUser!.phoneNumber}');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,80 +32,97 @@ class _ActivityScreenRiderState extends State<ActivityScreenRider> {
           style: AppTextStyles.heading20Bold,
         ),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        shrinkWrap: true,
-        padding: EdgeInsets.symmetric(
-          horizontal: 3.w,
-          vertical: 2.h,
-        ),
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (context, index) {
-          return Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 1.7.h,
-            ),
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: index == 9 ? transparent : greyShade3,
-                ),
+      body: StreamBuilder(
+        stream: tripHistoryRef.onValue,
+        builder: (context, event) {
+          if (event.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: black,
               ),
-            ),
-            height: 11.4.h,
-            width: 94.w,
-            child: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 0.5.h,
-                    horizontal: 1.w,
-                  ),
-                  height: 8.h,
-                  width: 8.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                      8.sp,
+            );
+          }
+          if (event.data != null) {
+            return FirebaseAnimatedList(
+                query: tripHistoryRef,
+                itemBuilder: (context, snapshot, animation, index) {
+                  RideRequestModel currentRideData = RideRequestModel.fromMap(
+                    jsonDecode(jsonEncode(snapshot.value))
+                        as Map<String, dynamic>,
+                  );
+                  return Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 1.7.h,
                     ),
-                    color: greyShadeButton,
-                    image: const DecorationImage(
-                      image: AssetImage(
-                        'assets/images/vehicle/car.png',
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: index == 9 ? transparent : greyShade3,
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  width: 5.w,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        '33, 2nd Cross Road',
-                        style: AppTextStyles.small12Bold,
-                        maxLines: 2,
-                      ),
-                      Text(
-                        DateFormat('dd MMM, kk:mm a').format(
-                          DateTime.now(),
+                    height: 11.4.h,
+                    width: 94.w,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 0.5.h,
+                            horizontal: 1.w,
+                          ),
+                          height: 8.h,
+                          width: 8.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              8.sp,
+                            ),
+                            color: greyShadeButton,
+                            image: const DecorationImage(
+                              image: AssetImage(
+                                'assets/images/vehicle/car.png',
+                              ),
+                            ),
+                          ),
                         ),
-                        style: AppTextStyles.small10.copyWith(
-                          color: black87,
+                        SizedBox(
+                          width: 5.w,
                         ),
-                      ),
-                      Text(
-                        '150:00',
-                        style: AppTextStyles.small10.copyWith(
-                          color: black87,
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                '33, 2nd Cross Road',
+                                style: AppTextStyles.small12Bold,
+                                maxLines: 2,
+                              ),
+                              Text(
+                                DateFormat('dd MMM, kk:mm a').format(
+                                  DateTime.now(),
+                                ),
+                                style: AppTextStyles.small10.copyWith(
+                                  color: black87,
+                                ),
+                              ),
+                              Text(
+                                '150:00',
+                                style: AppTextStyles.small10.copyWith(
+                                  color: black87,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                });
+          }
+          return Center(
+            child: Text(
+              'No Trips Made',
+              style: AppTextStyles.body14Bold.copyWith(color: grey),
             ),
           );
         },
